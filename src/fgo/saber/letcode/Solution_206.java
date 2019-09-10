b50ad74ee48f8bdfe9d1d2a11bc86c3783d4b51d
Saber <494309953@qq.com>
2019-09-10 10:27:01 +0800

    提交一道题目

--- src/fgo/saber/letcode/Solution_206.java
+++ src/fgo/saber/letcode/Solution_206.java
@@ -4,5 +4,66 @@ package fgo.saber.letcode;
  * @author zq
  * @date 2019/9/10
  */
+
+/**
+ * 反转一个单链表。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ */
 public class Solution_206 {
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+//        listNode.next = new ListNode(2);
+//        listNode.next.next = new ListNode(3);
+//        listNode.next.next.next = new ListNode(4);
+//        listNode.next.next.next.next = new ListNode(5);
+
+        ListNode listNode1 = reverseList(listNode);
+        System.out.println(listNode1);
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode temp = head.next;
+
+        head.next = null;
+
+        ListNode result = trans(temp);
+
+        temp.next = head;
+
+        return result;
+    }
+
+    private static ListNode trans(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        if (head.next == null) {
+            return head;
+        }
+        ListNode temp = head.next;
+
+        ListNode result = trans(temp);
+
+        temp.next = head;
+
+        return result;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
 }
